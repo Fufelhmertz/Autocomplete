a7599a655a0e34dd7aa8e8cfadfcb64043cbe97a
Fufelhmertz <big-andru@mail.ru>
2020-05-15 18:28:02 +0300

    AllFiles

---
+++ AutocompleteTask.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Autocomplete
+{
+    internal class AutocompleteTask
+    {
+        /// <returns>
+        /// Возвращает первую фразу словаря, начинающуюся с prefix.
+        /// </returns>
+        /// <remarks>
+        /// Эта функция уже реализована, она заработает, 
+        /// как только вы выполните задачу в файле LeftBorderTask
+        /// </remarks>
+        public static string FindFirstByPrefix(IReadOnlyList<string> phrases, string prefix)
+        {
+            var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+            if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                return phrases[index];
+            
+            return null;
+        }
+
+        /// <returns>
+        /// Возвращает первые в лексикографическом порядке count (или меньше, если их меньше count) 
+        /// элементов словаря, начинающихся с prefix.
+        /// </returns>
+        /// <remarks>Эта функция должна работать за O(log(n) + count)</remarks>
+        public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
+        {
+           
+            var valueOfLeftBorder = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count)+1;
+            var countPhrases = GetCountByPrefix(phrases, prefix);
+            var result = new string[count];
+
+           if (count > countPhrases)
+                count = countPhrases;
+            
+            for(var i =0; i <  count; i++)
+                result[i] = phrases[valueOfLeftBorder+i];
+            return result;
+           
+        }
+
+        /// <returns>
+        /// Возвращает количество фраз, начинающихся с заданного префикса
+        /// </returns>
+        public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
+        {
+            var left = LeftBorderTask.GetLeftBorderIndex(phrases,prefix, -1, phrases.Count);
+            var right = RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count);
+            return right - left;
+        }
+    }
+    /*
+    [TestFixture]
+    public class AutocompleteTests
+    {
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenNoPhrases()
+        {
+            // ...
+            //CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        // ...
+
+        [Test]
+        public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
+        {
+            // ...
+            //Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        // ...
+    }*/
+}
